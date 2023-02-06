c6f6fa674e0d28fa01081d67f1f1c592c0aed8f0
hafidhsajid <hafidhsajid@gmail.com>
2023-02-06 18:39:47 +0700

    cleaning code

--- main.go
+++ main.go
@@ -3,15 +3,11 @@ package main
 import "golang/function"
 
 func main() {
-	// var input string
 	// fmt.Println(`Input : `)
 	// scanner := bufio.NewScanner(os.Stdin)
 	// scanner.Scan()
 	// input := scanner.Text()
 	// fmt.Scanf("%s", input)
-
-	// satu.ShowInput()
-	// fmt.Println(satu.ReverseString("asdf"))
 
 	// FunctionFindDuplicate("Hafidhsajid") // expect 2h2af2i2dsj
 	// function.FindDuplicate("Hafidh Sajid") // expect 2h2af2i2dsj
