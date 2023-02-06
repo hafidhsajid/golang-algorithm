68c460defbcb8a4d9a39482c99481c59e7669772
hafidhsajid <hafidhsajid@gmail.com>
2023-02-06 18:26:47 +0700

    find duplicate

---
+++ findDuplicate.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func FindDuplicate(input string) {
+
+	inputSlice := strings.Replace(input, " ", "", -1)
+	inputSlice = strings.ToLower(inputSlice)
+	var res string
+	for i := 0; i < len(inputSlice); i++ {
+		var count = 1
+		for j := i + 1; j < len(inputSlice); j++ {
+			if inputSlice[i] == inputSlice[j] {
+				count += 1
+				inputSlice = inputSlice[:j] + inputSlice[j+1:]
+			}
+		}
+		if count > 1 {
+			inputSlice = inputSlice[:i] + fmt.Sprint(count) + inputSlice[i:]
+			res = string(inputSlice)
+		}
+	}
+	fmt.Println(res)
+}
---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"golang/satu"
+)
+
+func main() {
+	// var input string
+	// fmt.Println(`Input : `)
+	// scanner := bufio.NewScanner(os.Stdin)
+	// scanner.Scan()
+	// input := scanner.Text()
+	// fmt.Scanf("%s", input)
+
+	// satu.ShowInput()
+	// fmt.Println(satu.ReverseString("asdf"))
+	satu.FindDuplicate(("Hafidhsajid")) // expect 2h2af2i2dsj
+	// satu.PrimeSearch()
+	// SwapVariable()
+
+}
